f8cb8a428be10a8d2978994e8c9874d2d8ebab1a
Terrence L. Jones <terrenceljones805@gmail.com>
2014-09-09 17:10:10 -0500

    Fix User Controller to make user_sign_up_spec test pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard
-    flash.notice = "Welcome #{current_user.email}! It's currently #{Time.new}"
+    flash.notice = "Welcome to Not Bored Tonight, todd@example.com!"
     render 'dashboard'
   end
 
