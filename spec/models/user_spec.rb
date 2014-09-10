cb67fce42cbba1b28c8a122ac31f8ff6773fd10c
Terrence L. Jones <terrenceljones805@gmail.com>
2014-09-10 09:42:01 -0500

    Add new User unit test to check uniqueness of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,6 @@ RSpec.describe User, :type => :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+  it { should validate_uniqueness_of :email }
+  
 end
