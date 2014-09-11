d010fab7bdce15ffaf5b576d797fe4ad7e89708b
Terrence L. Jones <terrenceljones805@gmail.com>
2014-09-11 11:56:50 -0500

    Create pending OmniAuth Facebook spec and helper files

---
+++ spec/features/user_facebook_authentication_spec.rb
@@ -0,0 +1,52 @@
+# Story
+#
+# As a user,
+# I would like the ability to sign up to use the site with my Facebook account
+# so that I don't have to create new credentials.
+#
+# Acceptance Criteria
+#
+#  * Successfully create account with Facebook credentials
+#  * Associated Facebook credentials with existing account, if user has already
+#  created an account with email and password
+
+feature "Facebook authentication" do
+
+  scenario "User signs up via. Facebook", do
+    sign_into_facebook_as "joe"
+    visit "/"
+    click_link "Sign In with Facebook"
+    expect(page).to     have_content("Successfully authenticated from Facebook account.")
+    expect(page).to     have_content("Sign Out")
+    expect(page).to_not have_content("Sign In")
+    click_link "Sign Out"
+    expect(page).to_not have_content("Sign Out")
+    expect(page).to     have_content("Sign In with Facebook")
+    User.where(
+      email: "joesmith@example.com",
+      facebook_uid: "12345",
+      facebook_username: "joe",
+      name: "Joe Smith",
+      github_access_token: ENV["FACEBOOK_ACCESS_TOKEN"],
+    ).count.should == 1
+    User.count.should == 1
+  end
+
+  scenario "Student can log in and log out with Facebook" do
+    Fabricate(:user,
+      facebook_uid: "12345",
+      email: "joe@example.com",
+      facebook_username: "joe")
+    sign_into_facebook_as "joe"
+    visit "/"
+    click_link "Sign In with Facebook"
+    expect(page).to     have_content("Successfully authenticated from Facebook account.")
+    expect(page).to     have_content("Sign Out")
+    expect(page).to_not have_content("Sign In")
+    click_link "Sign Out"
+    expect(page).to_not have_content("Sign Out")
+    expect(page).to     have_content("Sign In with Github")
+    User.count.should == 1
+  end
+
+end
---
+++ spec/support/feature/authentication_helper.rb
@@ -0,0 +1,46 @@
+module Features
+  module AuthenticationHelpers
+
+    def sign_into_facebook_as(user_or_username, token: nil)
+      if user_or_username.is_a? User
+        user = user_or_username
+      end
+
+      token = user_or_username.try(:facebook_access_token) || token || ENV["FACEBOOK_ACCESS_TOKEN"]
+      username = user_or_username.try(:Facebook_username) || user_or_username
+      uid = user_or_username.try(:facebook_uid) || '12345'
+      email = user.try(:email) || "#{username}smith@example.com"
+
+      photo_url = "http://graph.facebook.com/#{uid}/picture?type=square"
+      @default_image ||= File.read(Rails.root.join('spec', 'support', 'files', 'cartoon-ninja.jpeg'))
+
+      stub_request(:get, photo_url).
+        to_return( body: @default_image,
+                  :status   => 200,
+                  :headers  => { 'Content-Type' => "image/jpeg; charset=UTF-8" } )
+
+      OmniAuth.config.add_mock(:facebook, {
+        uid: uid,
+        credentials: {
+          token: token
+        },
+        info: {
+          nickname: username,
+          email: email,
+          name: "#{username.capitalize} Smith",
+          image: photo_url,
+        },
+        extra: {
+          raw_info: {
+            location: 'San Francisco',
+            gravatar_id: '123456789'
+          }
+        }
+      })
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include Features::AuthenticationHelpers, type: :feature
+end
