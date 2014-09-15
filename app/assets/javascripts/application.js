3248f52485a8cf44788a070744378f4e421b1529
Terrence L. Jones <terrenceljones805@gmail.com>
2014-09-15 14:13:50 -0500

    Remove code from Application.js to remedy errors seen in browser console

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require foundation
 //= require turbolinks
 //= require_tree .
-$(function(){ $(document).foundation(); });
