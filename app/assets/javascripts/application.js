3fb764521d2f503bf33e667c2ea1b7a524526359
Terrence L. Jones <terrenceljones805@gmail.com>
2014-09-18 02:11:35 -0500

    Add scripting for Foundation to fix issue with carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require foundation
 //= require turbolinks
 //= require_tree .
+$(function(){ $(document).foundation(); });
