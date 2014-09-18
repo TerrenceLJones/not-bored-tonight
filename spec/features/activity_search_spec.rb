89b3d3f016218f73a8b8430bca52c9653b108904
Terrence L. Jones <terrenceljones805@gmail.com>
2014-09-18 02:09:41 -0500

    Create initial spec for activity search page

--- spec/features/activity_search_spec.rb
+++ spec/features/activity_search_spec.rb
@@ -6,28 +6,20 @@ feature "Activity Search Page" do
     expect(page).to have_content("Find Activities")
   end
 
-  scenario "should have search navigation", js: :true do
+  scenario "should have search navigation" do
     visit "/"
     click_link "Find Activities"
-    expect(page).to have_link("Stay In")
-    expect(page).to have_link("Go Out")
-    expect(page).to have_link("Television")
-    expect(page).to_not have_link("Sports")
-  end
-
-  scenario "should show hidden search items", js: :true do
-    visit "/"
-    click_link "Find Activities"
-    click_link "Go Out"
+    expect(page).to have_css(".search-container__navigation")
     expect(page).to have_link("Sports")
-    expect(page).to_not have_link("Television")
+    expect(page).to have_link("Food")
   end
 
   scenario "should show search results", js: :true do
     visit "/"
     click_link "Find Activities"
-    click_link "Television"
-    expect(find(".search-container__search-results")).to have_content("This is a test!")
+    click_link "Sport"
+    expect(find(".search-container__search-results")).to have_content("Title")
+    expect(page).to have_css(".search-results__sports")
   end
 
 end
