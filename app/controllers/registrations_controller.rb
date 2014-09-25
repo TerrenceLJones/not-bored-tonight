abd538e1ddedbcf971a8605028e55d4be688c3e7
Terrence L. Jones <terrenceljones805@gmail.com>
2014-09-25 18:55:51 -0500

    Implement code to allow user who sign in with Facebook to not need a password to update their profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def update_resource(resource, params)
+    if current_user.provider == "facebook"
+      params.delete("current_password")
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
+
   protected
 
   def after_sign_up_path_for(resource)
@@ -12,5 +21,6 @@ class RegistrationsController < Devise::RegistrationsController
     flash.notice = "Your account has been successfully updated!"
     dashboard_path
   end
+
 
 end
